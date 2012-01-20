825785c20da9aa50c23e6a48618642c86c3b710d
Daniel Tull <dt@danieltull.co.uk>
2012-01-20 12:31:14 +0000

    Initial Commit

---
+++ DCTFoursquare/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DCTFoursquare
+//
+//  Created by Daniel Tull on 20.01.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DCTFoursquare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DCTFoursquare
+//
+//  Created by Daniel Tull on 20.01.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
